8d3ad5eb64ec824b9e78ef685116dc2d22fd86c6
dallasschauer <wildfan18568@gmail.com>
2024-03-26 20:56:16 -0500

    Updates

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ scene.add(pfp);
 
 pfp.position.x = -17;
 pfp.position.y = 28;
-pfp.position.z = 760;
+pfp.position.z = 765;
 
 const kbTexture = new THREE.TextureLoader().load('images/keyboard.png');
 
@@ -116,7 +116,7 @@ scene.add(kb);
 
 kb.position.x = -15;
 kb.position.y = 28;
-kb.position.z = 1436;
+kb.position.z = 1440;
 
 const ghTexture = new THREE.TextureLoader().load('images/github.PNG');
 
@@ -129,7 +129,7 @@ scene.add(gh);
 
 gh.position.x = -12;
 gh.position.y = 28;
-gh.position.z = 2100;
+gh.position.z = 2120;
 
 function makeIntroVisible() {
   const instruction = document.getElementById("instructions");
